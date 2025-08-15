796ea4dedb69ff5207f0afec387cdb3e954351cb
bee bees <ahmedyoussefabbes@gmail.com>
2025-08-15 10:27:56 +0000

    Make the following changes:
    On the page src/app/(dashboard)/accueil/pag

--- src/app/(dashboard)/accueil/page.tsx
+++ src/app/(dashboard)/accueil/page.tsx
@@ -80,7 +80,7 @@ export default function AccueilZenPage() {
             </Button>
         ) : (
             <Button asChild variant="outline">
-                <Link href="/">
+                <Link href="/login">
                      <LogIn className="mr-2 h-4 w-4" />
                     Se connecter
                 </Link>
